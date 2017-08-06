a1484e8577457ee401cc409169bb4cf3d319e8dd
sonumeewa <sonumeewa@gmail.com>
2017-08-06 13:29:33 +0530

    Set DEBUG=False for HEROKU

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ul12pk1cgqi62(q-$g5sfae(*d4s7(^e*fb^chzm5&e^r=ex)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
